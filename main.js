f31612e37b5fe726d8c256226cb48ed71545fdd9
ogoras <skus30@wp.pl>
2024-09-29 17:47:59 +0200

    Recorder class

--- main.js
+++ main.js
@@ -1,77 +1,15 @@
 import { drawWaveform } from './drawWaveform.js';
 import { drawFormants } from './drawFormants.js';
 import { soundToFormant } from './sound_to_formant/formant.js';
-//import { exampleSamples } from './sound_to_formant/example_samples.js';
+import { AudioRecorder } from './recorder.js';
 
-let stream = null;
-try {
-    stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-}
-catch (error) {
-    console.log("An error occured: " + error);
-}
-let audioCtx = null, source = null, audioBufferData = [], recorderNode = null;
-let initialized = false, recording = false;
-
-let recordingIndicator = document.querySelector('.recording-indicator');
-
-async function init() {
-    audioCtx = new AudioContext();
-    source = audioCtx.createMediaStreamSource(stream);
-    await audioCtx.audioWorklet.addModule('recorder-worklet-processor.js');
-    recorderNode = new AudioWorkletNode(audioCtx, 'recorder-processor');
-    recorderNode.port.onmessage = (event) => {
-        const inputData = event.data;
-        audioBufferData.push(...inputData);
-    };
-}
-
-async function startRecording() {
-    let hintElements = document.getElementsByClassName("record-press-me");
-    for (let i = 0; i < hintElements.length; i++) {
-        hintElements[i].style.display = "none";
-    }
-
-    if (recording) return;
-    audioBufferData = [];
-    if (!initialized) {
-        await init();
-        initialized = true;
-    }
-    // Step 7: Connect the source to the AudioWorkletNode and the node to the destination
-    source.connect(recorderNode);
-    recordingIndicator.style.backgroundColor = 'red';
-    recording = true;
-}
-
-function stopRecording() {
-    if (!recording) return;
-    source.disconnect();
-    recordingIndicator.style.backgroundColor = '#ff000000';
-    recording = false;
-}
-
-let recordButton = document.querySelector('.record-button');
-// mouse, touch, or spacebar
-recordButton.addEventListener('mousedown', startRecording);
-recordButton.addEventListener('touchstart', startRecording);
-addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        startRecording();
-    }
-});
-recordButton.addEventListener('mouseup', stopRecording);
-recordButton.addEventListener('touchend', stopRecording);
-addEventListener('keyup', (event) => {
-    if (event.code === 'Space') {
-        stopRecording();
-    }
-});
+const audioRecorder = new AudioRecorder();
 
 const bufferLength = 1024;
 const dataArray = new Float32Array(bufferLength);
 
 function draw() {
+    const audioBufferData = audioRecorder.audioBufferData;
     if (audioBufferData.length < bufferLength / 128) {
         requestAnimationFrame(draw);
         return;
@@ -83,8 +21,8 @@ function draw() {
     const halfdt_window = 0.025;
     const preemphasisFrequency = 50;
     const samples = [...audioBufferData];
-    audioBufferData = [];
-    const formants = soundToFormant([...samples], audioCtx.sampleRate, dt, nFormants, maximumFrequency, halfdt_window, preemphasisFrequency);
+    audioRecorder.flush();
+    const formants = soundToFormant([...samples], audioRecorder.sampleRate, dt, nFormants, maximumFrequency, halfdt_window, preemphasisFrequency);
     //const formants = soundToFormant(exampleSamples, audioCtx.sampleRate, dt, nFormants, maximumFrequency, halfdt_window, preemphasisFrequency);
     for (let i = 0; i < formants.length; i++) {
         if (formants[i].formant.length >= 2) {
---
+++ recorder.js
@@ -0,0 +1,77 @@
+export class AudioRecorder {
+    stream = null;
+    audioCtx = null; source = null; audioBufferData = []; recorderNode = null;
+    initialized = false; recording = false;
+
+    recordingIndicator = document.querySelector('.recording-indicator');
+    recordButton = document.querySelector('.record-button');
+
+    get sampleRate() { 
+        return this.audioCtx ? this.audioCtx.sampleRate : null;
+    }
+
+    constructor() {
+        let startCallback = this.startRecording.bind(this);
+        this.recordButton.addEventListener('mousedown', startCallback);
+        this.recordButton.addEventListener('touchstart', startCallback);
+        addEventListener('keydown', (event) => {
+            if (event.code === 'Space') {
+                startCallback();
+            }
+        });
+        let stopCallback = this.stopRecording.bind(this);
+        this.recordButton.addEventListener('mouseup', stopCallback);
+        this.recordButton.addEventListener('touchend', stopCallback);
+        addEventListener('keyup', (event) => {
+            if (event.code === 'Space') {
+                stopCallback();
+            }
+        });
+    }
+
+    async init() {
+        try {
+            this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        }
+        catch (error) {
+            console.log("An error occured: " + error);
+        }
+        this.audioCtx = new AudioContext();
+        this.source = this.audioCtx.createMediaStreamSource(this.stream);
+        await this.audioCtx.audioWorklet.addModule('recorder-worklet-processor.js');
+        this.recorderNode = new AudioWorkletNode(this.audioCtx, 'recorder-processor');
+        this.recorderNode.port.onmessage = (event) => {
+            const inputData = event.data;
+            this.audioBufferData.push(...inputData);
+        };
+    }
+
+    async startRecording() {
+        let hintElements = document.getElementsByClassName("record-press-me");
+        for (let i = 0; i < hintElements.length; i++) {
+            hintElements[i].style.display = "none";
+        }
+
+        if (this.recording) return;
+        this.audioBufferData = [];
+        if (!this.initialized) {
+            await this.init();
+            this.initialized = true;
+        }
+        // Step 7: Connect the source to the AudioWorkletNode and the node to the destination
+        this.source.connect(this.recorderNode);
+        this.recordingIndicator.style.backgroundColor = 'red';
+        this.recording = true;
+    }
+
+    stopRecording() {
+        if (!this.recording) return;
+        this.source.disconnect();
+        this.recordingIndicator.style.backgroundColor = '#ff000000';
+        this.recording = false;
+    }
+
+    flush() {
+        this.audioBufferData = [];
+    }
+}
