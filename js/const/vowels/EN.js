1220e57a4dd8fb8bec213d06fb00b3ff888ac82b
ogoras <skus30@wp.pl>
2024-10-24 17:49:42 +0200

    updated the english color palette

--- js/const/vowels/EN.js
+++ js/const/vowels/EN.js
@@ -2,8 +2,8 @@ import Vowel from "../../data/Vowel.js";
 
 const vowels = [
     new Vowel({broad: "ɑ"}, "#ff0060"),
-    new Vowel({broad: "æ"}, "#fc5300"),
-    new Vowel({broad: "ɛ"}, "#faa500"),
+    new Vowel({broad: "æ"}, "#fa9000"),
+    new Vowel({broad: "ɛ"}, "#d09800"),
     new Vowel({broad: "i"}, "#00c800"),
     new Vowel({broad: "ɪ"}, "#006000"),
     new Vowel({broad: "u"}, "#0000ff"),
