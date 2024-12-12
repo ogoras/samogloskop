08d348d3cc8211739cd1e3d8954c7e47632746b5
ogoras <skus30@wp.pl>
2024-12-12 11:54:47 +0100

    v0.3.1

--- dist/const/version.js
+++ dist/const/version.js
@@ -1,8 +1,9 @@
 export const VERSION_MAJOR = 0, VERSION_MINOR = 3, // will only change when the data format changes
-PATCH = 0; // will change only with published commits, the in-beetween development versions are not counted
+PATCH = 1; // will change only with published commits, the in-beetween development versions are not counted
 // v0.0: introduced localStorage
 // v0.1: added Lobanov scaling
 // v0.2.0: new states: CONFIRM_VOWELS, INITIAL_FOREIGN, TRAINING, REPEAT_FOREIGN (_FOREIGN states not implemented yet)
 // v0.2.1: fixed a bug with Unknown state: TRAINING
 // v0.3.0: States and presets stored as integers are also supported, GATHERING_NATIVE is now a single state
 //          userVowels renamed to nativeVowels, added foreignInitial and foreignRepeat
+// v0.3.1: moved codebase to TypeScript, fixed some potential bugs
--- src/const/version.ts
+++ src/const/version.ts
@@ -1,6 +1,6 @@
 export const VERSION_MAJOR = 0,
     VERSION_MINOR = 3,  // will only change when the data format changes
-    PATCH = 0;  // will change only with published commits, the in-beetween development versions are not counted
+    PATCH = 1;  // will change only with published commits, the in-beetween development versions are not counted
 
 // v0.0: introduced localStorage
 // v0.1: added Lobanov scaling
@@ -8,3 +8,4 @@ export const VERSION_MAJOR = 0,
 // v0.2.1: fixed a bug with Unknown state: TRAINING
 // v0.3.0: States and presets stored as integers are also supported, GATHERING_NATIVE is now a single state
 //          userVowels renamed to nativeVowels, added foreignInitial and foreignRepeat
+// v0.3.1: moved codebase to TypeScript, fixed some potential bugs
