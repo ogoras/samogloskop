add84ece9e865141ba4f8f25f0890d5841500bcb
ogoras <skus30@wp.pl>
2024-12-13 15:35:53 +0100

    DataDownloadView

--- dist/view/DataDownloadView.js
+++ dist/view/DataDownloadView.js
@@ -1,3 +1,29 @@
 import View from "./View.js";
 export default class DataDownloadView extends View {
+    constructor(controller) {
+        super(controller);
+        // start with a clean slate
+        document.body.innerHTML = "";
+        const parentContainer = document.createElement("div");
+        parentContainer.classList.add("center");
+        document.body.appendChild(parentContainer);
+        this.container = document.createElement("div");
+        this.container.classList.add("stack");
+        parentContainer.appendChild(this.container);
+        // Add a title
+        const title = document.createElement("h1");
+        title.innerHTML = "Jeszcze jeden krok!";
+        this.container.appendChild(title);
+        // Add a paragraph
+        const p = document.createElement("p");
+        p.innerHTML = `Aby zakończyć swój udział w badaniu, pobierz swoje dane i załącz je w ankiecie.`;
+        this.container.appendChild(p);
+        // Add a button to download the data
+        const downloadButton = document.createElement("button");
+        downloadButton.innerHTML = "Pobierz dane";
+        downloadButton.onclick = () => {
+            controller.downloadData();
+        };
+        this.container.appendChild(downloadButton);
+    }
 }
