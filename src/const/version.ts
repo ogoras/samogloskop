48d39a7d483721617c974cce44f92c836bfb6c4a
ogoras <skus30@wp.pl>
2024-12-28 14:20:57 +0100

    0.4.4 fix

--- src/const/version.ts
+++ src/const/version.ts
@@ -1,6 +1,6 @@
 export const VERSION_MAJOR = 0,
     VERSION_MINOR = 4,  // will only change when the data format changes
-    PATCH = 3;  // will change only with published commits, the in-beetween development versions are not counted
+    PATCH = 4;  // will change only with published commits, the in-beetween development versions are not counted
 
 // v0.0: Introduced localStorage
 // v0.1: Added Lobanov scaling
