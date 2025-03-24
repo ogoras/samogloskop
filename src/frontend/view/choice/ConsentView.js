be7fd5ba841650b2f04c8b72a817754d00403e08
ogoras <skus30@wp.pl>
2025-03-24 17:18:23 +0100

    embedded instruction video at welcome page

--- src/frontend/view/choice/ConsentView.js
+++ src/frontend/view/choice/ConsentView.js
@@ -4,9 +4,24 @@ import ConsentComponent from "../../components/choice/ConsentComponent.js";
 export default class ConsentView extends ChoiceView {
     constructor(controller) {
         super(controller, ConsentComponent);
+
+        this.h2 = document.createElement("h2");
+        this.h2.innerHTML = "Witaj w aplikacji <b>Samogłoskop</b>! Na początek obejrzyj krótki filmik instruktażowy.";
+        this.container.appendChild(this.h2);
+        this.container.insertBefore(this.h2, this.container.firstChild);
+
+        this.iframe = document.createElement("iframe");
+        this.iframe.src = "https://www.youtube.com/embed/u7rLlYYY-es";
+        this.iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+        this.iframe.referrerPolicy = "strict-origin-when-cross-origin";
+        this.iframe.allowFullscreen = true;
+
+        this.h2.after(this.iframe);
     }
 
     destroy() {
+        this.h2.remove();
+        this.iframe.remove();
         super.destroy();
     }
 }
