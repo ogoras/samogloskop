a180f79e28fb25c7ea4d558878dd6120ff92444a
ogoras <skus30@wp.pl>
2025-04-27 17:05:01 +0200

    various mixed effects models

--- data_processing/results_analysis/linear_model.py
+++ data_processing/results_analysis/linear_model.py
@@ -1,34 +1,50 @@
-import pandas as pd, bambi as bmb, arviz as az, numpy as np
-import statsmodels.api as sm
+import pandas as pd, numpy as np
 import statsmodels.formula.api as smf
 
-FOLDER_ENDING = '_phases1&2'
+print("Libraries imported")
+
+FOLDER_ENDING = '' # '_phases1&2'
 
 # load data
 distances_data = pd.read_csv(f'data/results_output{FOLDER_ENDING}/distances_long_format.csv')
 print("Data loaded")
-distances_data['distance_to_target'] = np.log(distances_data['distance_to_target'])   # log-transform to make it homoscedastic
+distances_data['distance_to_target'] = np.log10(distances_data['distance_to_target']) / 2   # log-transform to make it homoscedastic
 
-# model = bmb.Model("distance_to_target ~ isControlGroup * isPre + (1|C(no)) + (1|vowel)", distances_data)
-model = smf.mixedlm("distance_to_target ~ isControlGroup * isPre", distances_data, groups=distances_data["no"])
-print("Model created")
-# model.fit()
-result = model.fit()
-print("Model fit")
-# model.summary()
-print(result.summary())
+def fit_data(show_residuals=False, data=distances_data, formula="distance_to_target ~ isControlGroup * isPre"):
+    # model = bmb.Model("distance_to_target ~ isControlGroup * isPre + (1|C(no)) + (1|vowel)", distances_data)
+    model = smf.mixedlm(formula, data, groups=data["no"])
+    print("Model created")
+    # model.fit()
+    result = model.fit()
+    print("Model fit")
+    # model.summary()
+    print(result.summary())
 
-# compute residuals
-residuals = result.resid
-print("Residuals computed")
-# compute fitted values
-fitted_values = result.fittedvalues
-print("Fitted values computed")
-# plot residuals vs fitted values
-import matplotlib.pyplot as plt
-plt.scatter(fitted_values, residuals)
-plt.xlabel("Fitted values")
-plt.ylabel("Residuals")
-plt.title("Residuals vs Fitted values")
-plt.axhline(0, color='red', linestyle='--')
-plt.show()
+    if show_residuals:
+        # compute residuals
+        residuals = result.resid
+        print("Residuals computed")
+        # compute fitted values
+        fitted_values = result.fittedvalues
+        print("Fitted values computed")
+        # plot residuals vs fitted values
+        import matplotlib.pyplot as plt
+        plt.scatter(fitted_values, residuals)
+        plt.xlabel("Fitted values")
+        plt.ylabel("Residuals")
+        plt.title("Residuals vs Fitted values")
+        plt.axhline(0, color='red', linestyle='--')
+        plt.show()
+
+    # calculate eta-squared value
+    predicted_variance = np.var(result.fittedvalues)
+    total_variance = np.var(distances_data['distance_to_target'])
+    eta_squared = predicted_variance / total_variance
+    print("Eta-squared:")
+    print(eta_squared)
+
+    return result
+
+vowels = 'iɪɛæɑʌɔʊu'
+
+fit_data(True)
